c7bfdb275f99bd946cea92a7bdced9be23b32101
Ignacio Piñeyro <juanpineyrob@gmail.com>
2025-05-13 18:23:37 -0300

    Saving Order

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dscommerce.controllers;
+
+import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.services.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
+        OrderDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert (@Valid @RequestBody OrderDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+}
