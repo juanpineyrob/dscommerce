b2f14beee9bb2eb4310ed7182dc64be72ce08289
Ignacio Piñeyro <juanpineyrob@gmail.com>
2025-05-23 17:32:54 -0300

    GET categories

--- src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
@@ -1,8 +1,9 @@
 package com.devsuperior.dscommerce.repositories;
 
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
